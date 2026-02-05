8e63d6fdbe0ae30561a30f62fdd6e244d7bc6f37
adolfo <adolfo@innovation-sandbox.com>
2026-02-05 20:12:57 +0000

    trying to Derive Address from Mnemonic

--- src/routes/main_routes.ts
+++ src/routes/main_routes.ts
@@ -1,32 +1,23 @@
 import { Router } from 'express';
-import {
-    getHomePage,
-    getWalletPage,
-    getPrivateKeyToAddressPage,
-    getMnemonicToPrivateKeyPage,
-    getCoinTypesPage,
-    getTransferPage
-} from '../controllers/main_controller';
-import {
-    getAddressFromPrivateKey,
-    getAddressFromMnemonic,
-    createWallet
-} from '../controllers/address_controller';
-import { transferBtc } from '../controllers/transfer_controller';
+import { getCoinTypesPage, getHomePage, getMnemonicToPrivateKeyPage, getPrivateKeyToAddressPage, getTransferPage } from '../controllers/main_controller';
+import { createWallet, getAddressFromMnemonic, postPrivateKeyToAddressPage } from '../controllers/address_controller';
 
 const router = Router();
 
-// Page rendering routes
 router.get('/', getHomePage);
-router.get('/wallet', getWalletPage);
-router.get('/private-key-to-address', getPrivateKeyToAddressPage);
-router.get('/mnemonic-to-private-key', getMnemonicToPrivateKeyPage);
+
 router.get('/cointypes', getCoinTypesPage);
+
+router.get('/mnemonic-to-private-key', getMnemonicToPrivateKeyPage);
+
+router.get('/private-key-to-address', getPrivateKeyToAddressPage);
+
 router.get('/transfer', getTransferPage);
 
-// API routes
-router.post('/api/wallet', createWallet);
-router.post('/api/get-address-from-mnemonic', getAddressFromMnemonic);
-router.post('/transfer-btc', transferBtc);
+router.post('/mnemonic-to-private-key', getAddressFromMnemonic);
+
+router.post('/api/create-wallet', createWallet);
+
+router.post('/private-key-to-address', postPrivateKeyToAddressPage);
 
 export default router;
